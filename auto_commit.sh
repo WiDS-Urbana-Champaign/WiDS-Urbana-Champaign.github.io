997e270ecd18cfa1bf4d495a7218175ea6a6a966
NAME <email>
2024-12-18 11:53:57 -0600

    syllabus

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MESSAGE="Auto-commit: $(date)"
-REPO_PATH="/Users/jnaiman/is445_bcubcg_fall2024"
+REPO_PATH="/Users/jnaiman/is445_obuobg_spring2025"
 cd $REPO_PATH
 /usr/bin/git add -A
 /usr/bin/git commit -m "$MESSAGE"
